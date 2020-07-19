a663971ccc64e3c902bfe111008075f7c44791dd
kolbymckeown <mckeownkolby@gmail.com>
2020-07-19 15:53:29 -0400

    header created

---
+++ realta/src/components/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+	return (
+		<>
+			<Wrapper>
+				<Title>Realta || Logistics & Warehousing</Title>
+				<Nav>
+                    <A href="#">Home</A>
+                    <A href="#">About</A>
+                    <A href="#">Contact</A>
+                    <A href="#">Products</A>
+                </Nav>
+			</Wrapper>
+		</>
+	);
+}
+
+const Wrapper = styled.div`
+    display: flex;
+    height: 15%;
+    justify-content: space-between;
+    margin-left: 40px;
+    margin-right: 40px;
+    align-items: center;
+`;
+
+const Title = styled.h1`
+    color: #39683c;
+`;
+
+const Nav = styled.nav`
+`;
+
+const A = styled.a`
+    text-decoration: none;
+    color: #39683c;
+    margin: 5px;
+`;
+
+export default Header;
