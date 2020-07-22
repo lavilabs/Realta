441fde36b7e987dfdf8624637b0b72a740135164
kolbymckeown <mckeownkolby@gmail.com>
2020-07-21 20:27:20 -0400

    data outline

---
+++ realta/src/data.js
@@ -0,0 +1,52 @@
+export default {
+	products: [
+		{
+			id: 1,
+			name: "placeholder",
+			image: "urlPlaceHolder",
+			price: 99,
+		},
+		{
+			id: 2,
+			name: "placeholder",
+			image: "urlPlaceHolder",
+			price: 99,
+		},
+		{
+			id: 3,
+			name: "placeholder",
+			image: "urlPlaceHolder",
+			price: 99,
+		},
+		{
+			id: 4,
+			name: "placeholder",
+			image: "urlPlaceHolder",
+			price: 99,
+		},
+		{
+			id: 5,
+			name: "placeholder",
+			image: "urlPlaceHolder",
+			price: 99,
+		},
+		{
+			id: 6,
+			name: "placeholder",
+			image: "urlPlaceHolder",
+			price: 99,
+		},
+		{
+			id: 7,
+			name: "placeholder",
+			image: "urlPlaceHolder",
+			price: 99,
+		},
+		{
+			id: 8,
+			name: "placeholder",
+			image: "urlPlaceHolder",
+			price: 99,
+		},
+	],
+};
